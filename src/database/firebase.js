84b3f58f5b10e67f2eb11e42494eb8c8ac067281
canutoilberto <canutoilberto@gmail.com>
2024-10-22 10:21:00 -0300

    [Fix] firebase config fixed

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  aapiKey: import.meta.env.VITE_API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
